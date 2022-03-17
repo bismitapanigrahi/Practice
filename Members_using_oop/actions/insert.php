dc01672d7c6a7f93efb60301f11d68f4649f8f4a
Panigrahi <bismita.panigrahi@accenture.com>
2022-03-17 19:47:58 +0530

    modified validation

--- Members_using_oop/actions/insert.php
+++ Members_using_oop/actions/insert.php
@@ -1,37 +1,33 @@
 <?php
     include 'connection.php';
-    class validator {
+    class validator extends dbconn {
         private $data;
         private $errors = [];
-        private static $fields = ['fname', 'lname', 'email', 'dob', 'gender'];
 
-        public function __construct($post_data) {
+        public function create($post_data) {
+            if(!isset($post_data['submit'])) {
+                $post_data['fname']="";
+                $post_data['lname']="";
+                $post_data['email']="";
+                $post_data['dob']="";
+                $post_data['gender']="";
+            }
+            
             $this->data=$post_data;
         }
         public function validateForm() {
-            foreach(self::$fields as $field) {
-                if(!array_key_exists($field, $this->data)) {
-                    trigger_error("$field is required");
-                    return;
-                }
-            }
+            
             $this->validatefname();
-            $this->validatelname();
             $this->validateemail();
             $this->validatedob();
-            $this->validategender();
+            $this->validategenderMale();
+            $this->validategenderFemale();
             return $this->errors;
         }
         private function validatefname() {
             $val=$this->data['fname'];
             if(empty($val)) {
-                $this->addError('fname', 'FirstName is required');
-            }
-        }
-        private function validatelname() {
-            $val=$this->data['lname'];
-            if(empty($val)) {
-                $val='';
+                $this->addError('fname', 'First Name is required');
             }
         }
         private function validateemail() {
@@ -46,104 +42,55 @@
                 $this->addError('dob', 'Date of Birth is required');
             }
         }
-        private function validategender() {
+        public function validategenderMale() {
             $val=$this->data['gender'];
             if(empty($val)) {
                 $this->addError('gender', 'Gender is required');
             }
+            if(isset($val)) {
+                if($val=='Male'){
+                    $val="checked";
+                }
+                return $val;
+            }
+            
+        }
+        public function validategenderFemale() {
+            $val=$this->data['gender'];
+            if(empty($val)) {
+                $this->addError('gender', 'Gender is required');
+            }
+            if(isset($val)) {
+                if($val=='Female'){
+                    $val="checked";
+                }
+                return $val;
+            }
+            
         }
         private function addError($key, $val) {
             $this->errors[$key]=$val;
         }
-    }
-    /* 
-    class insertRecord extends dbconn {
-        public function insert($post){
-            $fname=$post["fname"];
-            $lname=$post["lname"];
-            $email=$post["email"];
-            $dob=$post["dob"];
-            $gender=$post["gender"];
-            $sql="INSERT INTO requests (fname, lname, email, dob, gender) VALUES 
-            ('".$fname."', '".$lname."', '".$email."', '".$dob."', '".$gender."')";
-            $result=$this->conn->query($sql);
-            if($result) {
-                echo "<script>alert('Records Inserted Successfully');";
-                echo "window.location.href='http://localhost:8080/practice/programs_prac/Members_using_oop/display/allMembers.php';";
-                echo "</script>";
-            }
-            else {
-                echo "Failed";
-            }
-        }
-    }
-    
-    
-    
-    $fname= $lname= $email= $dob= $gender= $fnamerr= $emailerr= $doberr= $gendererr= $male= $female= '';
-    if(isset($_POST['submit'])) {
-        if(empty($_POST["fname"])){
-            $fnamerr= "Please enter your First Name";
-            $lname=$_POST["lname"];
-            $email=$_POST["email"];
-            $dob=$_POST["dob"];
-            if(isset($_POST['gender'])){
-                $gender=$_POST["gender"];
-                if($gender=='Male'){
-                    $male="checked";
-                }if($gender=='Female'){
-                    $female="checked";
+        public function insert() { 
+            if(!empty($this->data['fname']) && !empty($this->data['email']) && !empty($this->data['dob']) && !empty($this->data['gender'])){
+                $fname = $this->data['fname'];
+                $lname = $this->data['lname'];
+                $email = $this->data['email'];
+                $dob = $this->data['dob'];
+                $gender = $this->data['gender'];
+                //echo $fname."<br>".$lname."<br>".$email."<br>".$dob."<br>".$gender;
+                $sql="INSERT INTO requests (fname, lname, email, dob, gender) VALUES 
+                ('".$fname."', '".$lname."', '".$email."', '".$dob."', '".$gender."')";
+                $result=$this->conn->query($sql);
+                if($result) {
+                    echo "<script>alert('Records Inserted Successfully');";
+                    echo "window.location.href='http://localhost:8080/practice/programs_prac/Members_using_oop/display/allMembers.php';";
+                    echo "</script>";
+                }
+                else {
+                    echo "Failed";
                 }
             }
         }
-        elseif(empty($_POST["email"])){
-            $fname=$_POST["fname"];
-            $lname=$_POST["lname"];
-            $emailerr= "Please enter your email";
-            $dob=$_POST["dob"];
-            if(isset($_POST['gender'])){
-                $gender=$_POST["gender"];
-                if($gender=='Male'){
-                    $male="checked";
-                }if($gender=='Female'){
-                    $female="checked";
-                }
-            }
-        }
-        elseif(empty($_POST["dob"])){
-            $fname=$_POST["fname"];
-            $lname=$_POST["lname"];
-            $email=$_POST["email"];
-            $doberr= "Please enter your Date of Birth";
-            if(isset($_POST['gender'])){
-                $gender=$_POST["gender"];
-                if($gender=='Male'){
-                    $male="checked";
-                }if($gender=='Female'){
-                    $female="checked";
-                }
-            }
-        }
-        elseif(empty($_POST["gender"])){
-            $fname=$_POST["fname"];
-            $lname=$_POST["lname"];
-            $email=$_POST["email"];
-            $dob=$_POST["dob"];
-            $gendererr= "Please enter your gender";
-        }
-        else{
-            $fname=$_POST["fname"];
-            $lname=$_POST["lname"];
-            $email=$_POST["email"];
-            $dob=$_POST["dob"];
-            $gender=$_POST["gender"];
-            if($gender=='Male'){
-                $male="checked";
-            }if($gender=='Female'){
-                $female="checked";
-            } 
-            
-        }
     }
-    */
 ?>
